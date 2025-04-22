567ee20bb5cd1ce24a9a61a766114aa7d19fd24a
mirenja <gillian.kenyani@code.berlin>
2025-04-23 01:36:42 +0200

    feature: complete command response cycle

--- services/sshService.js
+++ services/sshService.js
@@ -1,35 +1,78 @@
 import { Client } from 'ssh2';
-import fs from 'fs'
 
 
-export function connectAndExecute({ host,SSH_PASS, command }) {
+
+// export function connectAndExecute({ host,password, command }) {
+//     return new Promise((resolve, reject) => {
+//       const conn = new Client()
+//       let output = ''
+     
+  
+//       conn.on('ready', () => {
+//         conn.exec(command, (err, stream) => {
+//           if (err) {
+//             conn.end()
+//             return reject(new Error('SSH exec error: ' + err.message))
+//           }
+  
+//           stream.on('data', data => output += data.toString());
+//           stream.stderr.on('data', data => output += 'ERROR: ' + data.toString())
+  
+//           stream.on('close', () => {
+//             conn.end()
+//             resolve(output.trim())
+//           })
+//         })
+//       }).on('error', (err) => {
+//         reject(new Error('SSH connection error: ' + err.message))
+//       }).connect({
+//         host,
+//         port: 22,
+//         username:"jill",
+//         password
+
+//       })
+//     })
+//   }
+
+
+export function connect({ host, username, password }) {
     return new Promise((resolve, reject) => {
-      const conn = new Client()
+        const conn = new Client()
+        
+        conn.on('ready', () => {
+            resolve(conn)  
+        }).on('error', (err) => {
+            reject(new Error('SSH connection error: ' + err.message))
+        }).connect({
+            host,
+            port: 22,
+            username,
+            password
+        })
+    })
+}
+
+
+export function sendCommand(conn, command) {
+    return new Promise((resolve, reject) => {
       let output = ''
   
-      conn.on('ready', () => {
-        conn.exec(command, (err, stream) => {
-          if (err) {
-            conn.end()
-            return reject(new Error('SSH exec error: ' + err.message))
-          }
+      conn.exec(command, (err, stream) => {
+        if (err) return reject(new Error('SSH exec error: ' + err.message))
   
-          stream.on('data', data => output += data.toString());
-          stream.stderr.on('data', data => output += 'ERROR: ' + data.toString())
+        stream.on('data', (data) => {
+          output += data.toString();
+        });
   
-          stream.on('close', () => {
-            conn.end()
-            resolve(output.trim())
-          })
+        stream.stderr.on('data', (data) => {
+          output += 'ERROR: ' + data.toString()
         })
-      }).on('error', (err) => {
-        reject(new Error('SSH connection error: ' + err.message))
-      }).connect({
-        host,
-        port: 22,
-        username:"jill",
-        password:'Peach1!'
-
+  
+        stream.on('close', () => {
+          conn.end()
+          resolve(output.trim())
+        })
       })
     })
   }
