eebd7c213016f16849ae5ef70f36a73e77056536
mirenja <gillian.kenyani@code.berlin>
2025-04-24 12:51:54 +0200

    fix: adetied the test models

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,5 +1,21 @@
 import request from 'supertest'
 import app from '../app.js'
+import  {MongoClient} from 'mongodb'
+
+let connection
+let db
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect(global.__MONGO_URI__, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        db = await connection.db(globalThis.__MONGO_DB_NAME__);
+      })
+
+    afterAll(async () => {
+      await connection.close();
+    })
 
 
 describe("Test the root path", () => {
@@ -21,6 +37,13 @@ describe("Test the root path", () => {
   describe("Test the sessions/show path", () => {
     test("It should response the GET method", async () => {
       const response = await request(app).get("/show")
+      expect(response.statusCode).toBe(200)
+    })
+  })
+
+  describe("Test the dashboard path", () => {
+    test("It should response the GET method", async () => {
+      const response = await request(app).get("/dashboard")
       expect(response.statusCode).toBe(200)
     })
   })
